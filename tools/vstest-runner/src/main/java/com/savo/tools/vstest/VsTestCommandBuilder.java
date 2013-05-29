b62b8cc14860c48671e1acbcee07cda47bce9389
Nick Gamroth <thebeekeeper@gmail.com>
2013-05-29 15:19:50 -0500

    Removed Gallio Plugin, adding vstest-runner tool

---
+++ tools/vstest-runner/src/main/java/com/savo/tools/vstest/VsTestCommandBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * vstest-runner
+ * Copyright (C) 2010 Jose Chillan, Alexandre Victoor and SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.savo.tools.vstest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.utils.command.Command;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ngamroth
+ * Date: 5/29/13
+ * Time: 11:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VsTestCommandBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(VsTestCommandBuilder.class);
+    String vstestCommand = "C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe";
+
+    private VsTestCommandBuilder() {
+    }
+
+    public Command toCommand() {
+        Command c = Command.create(vstestCommand);
+        return c;
+    }
+}
---
+++ tools/vstest-runner/src/main/java/com/savo/tools/vstest/VsTestRunner.java
@@ -0,0 +1,30 @@
+/*
+ * vstest-runner
+ * Copyright (C) 2010 Jose Chillan, Alexandre Victoor and SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.savo.tools.vstest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+    Runs tests using vstest.console
+ */
+public class VsTestRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(VsTestRunner.class);
+}
